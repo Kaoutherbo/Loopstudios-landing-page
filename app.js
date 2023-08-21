7decaab5fff8fe0f74221281394db3feb6137545
kaouther <kaoutherboutheldja@gmail.com>
2023-08-22 00:24:32 +0100

    Update style

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu');
 const closeBtn = document.querySelector('.close');
 const navMenu = document.querySelector('.list');
+const header = document.querySelector('header');
 let isClicked = true ;
 
 function toggleMenu() {
@@ -8,10 +9,12 @@ function toggleMenu() {
     navMenu.classList.add('active'); 
     menuBtn.style.display = 'none';
     closeBtn.style.display = 'block';
+    header.classList.add('active');
   } else {
     navMenu.classList.remove('active'); 
     menuBtn.style.display = 'block';
     closeBtn.style.display = 'none';
+    header.classList.remove('active');
   }
   isClicked = !isClicked;
 }
